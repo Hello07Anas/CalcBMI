6b921a604113a883924a788b5826efd8c7f231e6
anas <hello.anas07@gmail.com>
2024-07-20 18:21:51 +0300

    intial app and its design

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightOT: UILabel!
+    @IBOutlet weak var weightOT: UILabel!
+    @IBOutlet weak var heightProgressBar: UISlider!
+    @IBOutlet weak var weightProgressBar: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateBtn(_ sender: Any) {
+        
+    }
+    
 }
 
